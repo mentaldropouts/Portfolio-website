1acefef78f89d67469ebd00f562710ce1f230602
Taylor Payne <86752280+mentaldropouts@users.noreply.github.com>
2023-08-17 18:38:45 -0400

    Adding more HandleBars Functionality to Router and Landing

--- router.js
+++ router.js
@@ -5,10 +5,35 @@ const router = new express.Router();
 router.route('/')   
 .get((req,res)=>{
 console.log("Home was accessed");
-res.render('landing');
-})
-.post((req,res)=>{
-});
+res.render('landing',{
+        name: 'TAYLOR PAYNE',
+        title1: 'computer scientist',
+        title2: 'graphic designer',
+        button1: {
+                text: 'Resume',
+                url: './pdf/TaylorPayneResume.pdf',
+        },
+        button2: {
+                text: 'Transcript',
+                url: './pdf/Transcript.pdf',
+        },
+        button3: {
+                text: 'GitHub',
+                url: 'https://github.com/mentaldropouts'
+        },
+        button4: {
+                text: 'LinkedIn',
+                url: 'https://www.linkedin.com/in/taylorpaynedev/'
+        },
 
+
+})});
+
+
+router.route('/gallery')   
+.get((req,res)=>{
+console.log("gallery was accessed");
+res.render('gallery');
+});
 
 module.exports = router;

167ab2a5916166b1fb83d0ac7bf6a44387f591eb
Taylor.Payne <taylor.payne@uky.edu>
2023-09-16 15:13:57 -0400

    Major overhaul of entire site

---
+++ README.md
@@ -0,0 +1,3 @@
+# Taylor's Portfolio Website 
+## started in June 2023 
+Deployed on https://taylor-payne-portfolio.adaptable.app
---
+++ router.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = new express.Router();
+
+
+router.route('/')   
+.get((req,res)=>{
+console.log("Home was accessed");
+res.render('landing',{
+        name: 'Taylor Payne',
+        title1: 'University of Kentucky',
+        title2: 'Senior Computer Science Student',
+        button1: {
+                text: 'Resume',
+                url: './pdf/TaylorPayneResume.pdf',
+        },
+        button2: {
+                text: 'Transcript',
+                url: './pdf/Transcript.pdf',
+        },
+        button3: {
+                text: 'GitHub',
+                url: 'https://github.com/mentaldropouts'
+        },
+        button4: {
+                text: 'LinkedIn',
+                url: 'https://www.linkedin.com/in/taylorpaynedev/'
+        },
+
+
+})});
+router.route('/gallery')   
+.get((req,res)=>{
+console.log("gallery was accessed");
+res.render('gallery');
+});
+
+module.exports = router;
